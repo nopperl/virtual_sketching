92abfb96d30466247b2186aff0ad0fee7bc9fe39
nopperl <54780682+nopperl@users.noreply.github.com>
2023-07-23 23:35:53 +0200

    update entrypoint to generate svg files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+output_dir=outputs/sampling/clean_line_drawings__pretrain_clean_line_drawings
 cd /app/virtual_sketching
 ./download_model.sh
 find sample_inputs/clean_line_drawings -type f -name '*.png' -exec basename {} \; | xargs -L1 -I{} ./test_lineart.sh {}
+find $output_dir/seq_data -type f -name '*.npz' -exec python tools/svg_conversion.py --file {} \;
+mkdir -p $output_dir/svg
+find $output_dir/seq_data/* -type d -exec basename {} \; | xargs -L1 -I{} cp $output_dir/seq_data/{}/single.svg $output_dir/svg/{}.svg
